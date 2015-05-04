3c66775e8b599c05b2ad84cbb63d1140dace922d
Arif Islam <arif@dreamfactory.com>
2015-05-04 00:06:52 -0400

    updates

--- src/Services/LDAP.php
+++ src/Services/LDAP.php
@@ -21,8 +21,56 @@
 namespace DreamFactory\DSP\ADLdap\Services;
 
 use DreamFactory\Rave\Services\BaseRestService;
+use DreamFactory\Library\Utility\ArrayUtils;
+use DreamFactory\Library\Utility\Enums\Verbs;
 
 class LDAP extends BaseRestService
 {
+    protected $defaultRole;
 
+    protected $config;
+
+    /**
+     * @param array $settings
+     */
+    public function __construct( $settings = [ ] )
+    {
+        $verbAliases = [
+            Verbs::PUT   => Verbs::POST,
+            Verbs::MERGE => Verbs::PATCH
+        ];
+        ArrayUtils::set( $settings, "verbAliases", $verbAliases );
+        parent::__construct( $settings );
+
+        $this->config = ArrayUtils::get( $settings, 'config' );
+        $this->defaultRole = ArrayUtils::get($this->config, 'default_role');
+    }
+
+    protected function handlePOST()
+    {
+        if('session' === $this->resource)
+        {
+            $username = $this->getPayloadData('username');
+            $password = $this->getPayloadData('password');
+            $baseDn = ArrayUtils::get($this->config, 'base_dn');
+            $fullDn = 'cn='.$username.','.$baseDn;
+
+            $r = ldap_connect(ArrayUtils::get($this->config, 'host'));
+            ldap_set_option($r, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+            $auth = ldap_bind($r, $fullDn, $password);
+
+            if($auth)
+            {
+                // - Get user info
+                // - Create shadow user
+                // - Login with shadow user
+                // - return session info
+            }
+            else
+            {
+                // - Return exception
+            }
+        }
+    }
 }
