e17ab2c89b7e4b576a142f34a7ddfa475839cf1c
Lee Hicks <leehicks@dreamfactory.com>
2015-06-26 10:51:23 -0400

    changing over to use php 5.5 ::class notation instead of strings

--- src/Database/Seeds/DatabaseSeeder.php
+++ src/Database/Seeds/DatabaseSeeder.php
@@ -1,17 +1,21 @@
 <?php
 namespace DreamFactory\Core\ADLdap\Database\Seeds;
 
+use DreamFactory\Core\ADLdap\Models\LDAPConfig;
+use DreamFactory\Core\ADLdap\Services\ADLdap;
+use DreamFactory\Core\ADLdap\Services\LDAP;
 use DreamFactory\Core\Database\Seeds\BaseModelSeeder;
+use DreamFactory\Core\Models\ServiceType;
 
 class DatabaseSeeder extends BaseModelSeeder
 {
-    protected $modelClass = 'DreamFactory\\Core\\Models\\ServiceType';
+    protected $modelClass = ServiceType::class;
 
     protected $records = [
         [
             'name'           => 'adldap',
-            'class_name'     => "DreamFactory\\Core\\ADLdap\\Services\\ADLdap",
-            'config_handler' => "DreamFactory\\Core\\ADLdap\\Models\\LDAPConfig",
+            'class_name'     => ADLdap::class,
+            'config_handler' => LDAPConfig::class,
             'label'          => 'adLdap integration',
             'description'    => 'A service for supporting adLdap integration',
             'group'          => 'ldap',
@@ -19,8 +23,8 @@ class DatabaseSeeder extends BaseModelSeeder
         ],
         [
             'name'           => 'ldap',
-            'class_name'     => "DreamFactory\\Core\\ADLdap\\Services\\LDAP",
-            'config_handler' => "DreamFactory\\Core\\ADLdap\\Models\\LDAPConfig",
+            'class_name'     => LDAP::class,
+            'config_handler' => LDAPConfig::class,
             'label'          => 'LDAP integration',
             'description'    => 'A service for supporting OpenLdap integration',
             'group'          => 'ldap',
