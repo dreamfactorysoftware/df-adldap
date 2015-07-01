730ba26dc805d3855c5708adae4879cd2c307923
Arif Islam <arif@dreamfactory.com>
2015-07-01 10:55:30 -0400

    adding service config validation

--- src/Models/LDAPConfig.php
+++ src/Models/LDAPConfig.php
@@ -2,10 +2,27 @@
 namespace DreamFactory\Core\ADLdap\Models;
 
 use DreamFactory\Core\Models\BaseServiceConfigModel;
+use DreamFactory\Core\Exceptions\BadRequestException;
 
 class LDAPConfig extends BaseServiceConfigModel
 {
     protected $table = 'ldap_config';
 
     protected $fillable = ['service_id', 'default_role', 'host', 'base_dn', 'account_suffix'];
+
+    public static function validateConfig($config)
+    {
+        $validator = \Validator::make($config, [
+            'default_role' => 'required',
+            'host'         => 'required',
+            'base_dn'      => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $messages = $validator->messages()->getMessages();
+            throw new BadRequestException('Validation failed.', null, null, $messages);
+        }
+
+        return true;
+    }
 }
