5c2f3abad9fbe0d37174303d8eb8ecefa0aa2853
Arif Islam <arif@dreamfactory.com>
2015-07-21 11:24:17 -0400

    checking for ldap extension dependency

--- src/Models/LDAPConfig.php
+++ src/Models/LDAPConfig.php
@@ -1,17 +1,22 @@
 <?php
 namespace DreamFactory\Core\ADLdap\Models;
 
+use DreamFactory\Core\Components\RequireExtensions;
 use DreamFactory\Core\Models\BaseServiceConfigModel;
 use DreamFactory\Core\Exceptions\BadRequestException;
 
 class LDAPConfig extends BaseServiceConfigModel
 {
+    use RequireExtensions;
+
     protected $table = 'ldap_config';
 
     protected $fillable = ['service_id', 'default_role', 'host', 'base_dn', 'account_suffix'];
 
     public static function validateConfig($config)
     {
+        static::checkExtensions(['ldap']);
+
         $validator = \Validator::make($config, [
             'default_role' => 'required',
             'host'         => 'required',
