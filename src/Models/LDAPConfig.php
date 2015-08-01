b28c2adbdef9766520ac6338cd181f1a742f253a
Arif Islam <arif@dreamfactory.com>
2015-08-01 00:28:12 -0400

    fixing default role drop down on some service types like OAuth, AD/Ldap

--- src/Models/LDAPConfig.php
+++ src/Models/LDAPConfig.php
@@ -4,6 +4,7 @@ namespace DreamFactory\Core\ADLdap\Models;
 use DreamFactory\Core\Components\RequireExtensions;
 use DreamFactory\Core\Models\BaseServiceConfigModel;
 use DreamFactory\Core\Exceptions\BadRequestException;
+use DreamFactory\Core\Models\Role;
 
 class LDAPConfig extends BaseServiceConfigModel
 {
@@ -31,5 +32,31 @@ class LDAPConfig extends BaseServiceConfigModel
         }
 
         return true;
+    }
+
+    /**
+     * @param array $schema
+     */
+    protected static function prepareConfigSchemaField(array &$schema)
+    {
+        $roles = Role::whereIsActive(1)->get();
+        $roleList = [];
+
+        foreach($roles as $role){
+            $roleList[] = [
+                'label' => $role->name,
+                'name'  => $role->id
+            ];
+        }
+
+        parent::prepareConfigSchemaField($schema);
+
+        switch ($schema['name']) {
+            case 'default_role':
+                $schema['type'] = 'picklist';
+                $schema['values'] = $roleList;
+                $schema['description'] = 'Select a default role for users logging in with this AD/Ldap service type.';
+                break;
+        }
     }
 }
