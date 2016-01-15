b775539a5687af849d092fa3ce8b71f72ae1ecab
Lee Hicks <leehicks@dreamfactory.com>
2016-01-15 18:23:34 -0500

    On delete command 'restrict' only available on mysql, where it is the default.

--- database/migrations/2015_05_04_034605_create_adldap_tables.php
+++ database/migrations/2015_05_04_034605_create_adldap_tables.php
@@ -28,7 +28,9 @@ class CreateAdldapTables extends Migration
                 $t->integer('service_id')->unsigned()->primary();
                 $t->foreign('service_id')->references('id')->on('service')->onDelete('cascade');
                 $t->integer('default_role')->unsigned()->index();
-                $t->foreign('default_role')->references('id')->on('role')->onDelete('restrict');
+                // previously set to 'restrict' which isn't supported by all databases
+                // removing the onDelete clause gets the same behavior as No Action and Restrict are defaults.
+                $t->foreign('default_role')->references('id')->on('role');
                 $t->string('host');
                 $t->string('base_dn');
                 $t->string('account_suffix')->nullable();
