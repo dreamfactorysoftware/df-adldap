dreamfactorysoftware/df-adldap

commit ab15144155fe53d588662fdaf5daa122af7db648

Fixed ad group import issue after supporting has-one db relationship

src/Commands/ADGroupImport.php
@@ -83,11 +83,7 @@ class ADGroupImport extends Command
                         'name'                   => static::dnToRoleName($group['dn']),
                         'description'            => $group['description'],
                         'is_active'              => true,
-                        'role_adldap_by_role_id' => [
-                            [
-                                'dn' => $group['dn']
-                            ]
-                        ]
+                        'role_adldap_by_role_id' => ['dn' => $group['dn']]
                     ];
 
                     $this->info('|--------------------------------------------------------------------');

commit 5cd508eb51cd4479f4fd1c350a2546aaee1254e6

Fix AD group import TypeError on PHP 8.1+
The role_adldap_by_role_id relation value must be an array of arrays
(one record per related model). The flat array caused BaseModel to
iterate string values into Model::__construct(), which fatally fails
under PHP 8.1+ strict typing.

Regression introduced in ab15144 (2017).

src/Commands/ADGroupImport.php
@@ -83,7 +83,7 @@ class ADGroupImport extends Command
                         'name'                   => static::dnToRoleName($group['dn']),
                         'description'            => $group['description'],
                         'is_active'              => true,
-                        'role_adldap_by_role_id' => ['dn' => $group['dn']]
+                        'role_adldap_by_role_id' => [['dn' => $group['dn']]]
                     ];
 
                     $this->info('|--------------------------------------------------------------------');